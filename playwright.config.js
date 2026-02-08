840d481d3f5e26364b72567434f9cc6145e89fc9
jhhemnathtech-maker <jhhemnath.tech@gmail.com>
2026-02-08 11:16:34 +0530

    Enable HTML report for Jenkins

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,11 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
 
+  // ✅ ADD THIS REPORTER BLOCK
+  reporter: [
+    ['html', { open: 'never' }]
+  ],
+
   use: {
     headless: false,
     viewport: null,        // disables Playwright viewport
